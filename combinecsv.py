b3326d38d8d3af8070ee225dc5747b5f3108340a
AcidAcesen <acidfb01@gmail.com>
2023-10-16 11:25:22 +0800

    first commit

---
+++ combinecsv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read the first CSV file
+df1 = pd.read_csv('C:/Users/fadil/Downloads/apslogs(6).csv')
+
+# Read the second CSV file
+df2 = pd.read_csv('C:/Users/fadil/Downloads/15 LOG.csv')
+
+# Combine the DataFrames, keeping only unique rows
+combined_df = pd.concat([df1, df2]).drop_duplicates()
+
+# Save the combined DataFrame to a new CSV file
+combined_df.to_csv('combined.csv', index=False)
